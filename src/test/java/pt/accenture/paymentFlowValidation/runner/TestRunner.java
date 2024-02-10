011212c417df7e88dcd99b035e862439eb14abb9
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 <lgsduran@yahoo.com.br>
2024-02-10 22:47:31 +0000

    gitignore

--- src/test/java/pt/accenture/paymentFlowValidation/runner/TestRunner.java
+++ src/test/java/pt/accenture/paymentFlowValidation/runner/TestRunner.java
@@ -6,10 +6,11 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = "src/test/resources/features",
-		glue = {"pt.accenture.paymentFlowValidation.hooks", "pt.accenture.paymentFlowValidation.steps"},
-		tags =  "@order"
+@CucumberOptions
+		(
+			features = "src/test/resources/features",
+			glue = {"pt.accenture.paymentFlowValidation.hooks", "pt.accenture.paymentFlowValidation.steps"},
+			tags =  "@order"
 		)
 public class TestRunner {
 
